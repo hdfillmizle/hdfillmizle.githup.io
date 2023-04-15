b4aa0dc095bb2c0e3a3ae92f566e00588f998115
hdfillmizle <119707352+hdfillmizle@users.noreply.github.com>
2023-04-16 01:31:59 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,60 +4,60 @@ const cancelBtn = document.querySelector(".cancel-icon");
 const items = document.querySelector(".nav-items");
 const form = document.querySelector("form");
 menuBtn.onclick = () => {
-    items.classList.add("active");
-    menuBtn.classList.add("hide");
-    searchBtn.classList.add("hide");
-    cancelBtn.classList.add("show");
-}
+  items.classList.add("active");
+  menuBtn.classList.add("hide");
+  searchBtn.classList.add("hide");
+  cancelBtn.classList.add("show");
+};
 cancelBtn.onclick = () => {
-    items.classList.remove("active");
-    menuBtn.classList.remove("hide");
-    searchBtn.classList.remove("hide");
-    cancelBtn.classList.remove("show");
-    form.classList.remove("active");
-}
+  items.classList.remove("active");
+  menuBtn.classList.remove("hide");
+  searchBtn.classList.remove("hide");
+  cancelBtn.classList.remove("show");
+  form.classList.remove("active");
+};
 searchBtn.onclick = () => {
-    form.classList.add("active");
-    searchBtn.classList.add("hide");
-    cancelBtn.classList.add("show");
-}
+  form.classList.add("active");
+  searchBtn.classList.add("hide");
+  cancelBtn.classList.add("show");
+};
 
+const dropdownmenu = document.getElementsByClassName("dropdown-menu")[0];
+const submenu = document.getElementsByClassName("sub-menu")[0];
 
-const dropdownmenu = document.getElementsByClassName('dropdown-menu')[0]
-const submenu = document.getElementsByClassName('sub-menu')[0]
-
-dropdownmenu.addEventListener('click', () => {
-    submenu.classList.toggle('active');
+dropdownmenu.addEventListener("click", () => {
+  submenu.classList.toggle("active");
 });
 
 let calcScrollValue = () => {
-    let scrollProgress = document.getElementById("progress");
-    let progressValue = document.getElementById("progress-value");
-    let pos = document.documentElement.scrollTop;
-    let calcHeight =
-        document.documentElement.scrollHeight -
-        document.documentElement.clientHeight;
-    let scrollValue = Math.round((pos * 100) / calcHeight);
-    if (pos > 300) {
-        scrollProgress.style.display = "grid";
-    } else {
-        scrollProgress.style.display = "none";
-    }
-    scrollProgress.addEventListener("click", () => {
-        document.documentElement.scrollTop = 0;
-    });
-    scrollProgress.style.background = `conic-gradient(blue ${scrollValue}%, #424242 ${scrollValue}%)`;
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(blue ${scrollValue}%, #424242 ${scrollValue}%)`;
 };
 
 window.onscroll = calcScrollValue;
 window.onload = calcScrollValue;
 
+const dropdownbloklist = document.getElementsByClassName(
+  "drop-down-blok-list"
+)[0];
+const submenu2 = document.getElementsByClassName("sub-menu-2")[0];
 
-const dropdownbloklist = document.getElementsByClassName('drop-down-blok-list')[0]
-const submenu2 = document.getElementsByClassName('sub-menu-2')[0]
-
-dropdownbloklist.addEventListener('click', () => {
-    submenu2.classList.toggle('active');
+dropdownbloklist.addEventListener("click", () => {
+  submenu2.classList.toggle("active");
 });
 
 const btn_diva = document.getElementById("btn_diva");
@@ -65,13 +65,13 @@ const btn_divb = document.getElementById("btn_divb");
 const diva = document.getElementById("diva");
 const divb = document.getElementById("divb");
 
-btn_diva.addEventListener('click', ()=>{
-    diva.style.display='block';
-    divb.style.display='none';
+btn_diva.addEventListener("click", () => {
+  diva.style.display = "block";
+  divb.style.display = "none";
 });
-btn_divb.addEventListener('click', ()=>{
-    diva.style.display='none';
-    divb.style.display='block';
+btn_divb.addEventListener("click", () => {
+  diva.style.display = "none";
+  divb.style.display = "block";
 });
 
 const filmcatagori = document.getElementById("film-catagori");
@@ -81,33 +81,32 @@ const topdizi = document.getElementById("top-dizi");
 const loadMore = document.getElementById("loadMore");
 const More = document.getElementById("More");
 
-filmcatagori.addEventListener('click', ()=>{
-    filmcatagori.style.cssText += 'color:#000000;background-color:#eaad2c';
-    dizicatagori.style.cssText += 'color:#fff;background-color:#ffffff00';
-    topfilm.style.display='block';
-    topdizi.style.display='none';
+filmcatagori.addEventListener("click", () => {
+  filmcatagori.style.cssText += "color:#000000;background-color:#eaad2c";
+  dizicatagori.style.cssText += "color:#fff;background-color:#ffffff00";
+  topfilm.style.display = "block";
+  topdizi.style.display = "none";
 });
-dizicatagori.addEventListener('click', ()=>{
-    filmcatagori.style.cssText += 'color:#fff;background-color:#ffffff00';
-    dizicatagori.style.cssText += 'color:#000000;background-color:#eaad2c';
-    topfilm.style.display='none';
-    topdizi.style.display='block';
-});
-
-const footertoptextcontainer = document.getElementsByClassName('footer-top-text-container')[0]
-const textbtn = document.getElementsByClassName('text-btn')[0]
-
-textbtn.addEventListener('click', () => {
-    footertoptextcontainer.classList.toggle('active');;
+dizicatagori.addEventListener("click", () => {
+  filmcatagori.style.cssText += "color:#fff;background-color:#ffffff00";
+  dizicatagori.style.cssText += "color:#000000;background-color:#eaad2c";
+  topfilm.style.display = "none";
+  topdizi.style.display = "block";
 });
 
-function addOne()
-{
-    const loadMoredisplay = 
-    document.getElementById("loadMore-display");
-    const inside = loadMoredisplay.innerHTML;
-    const number = parseInt(inside)
-    const larger = number + 1;
-    loadMoredisplay.innerHTML = larger;
+const footertoptextcontainer = document.getElementsByClassName(
+  "footer-top-text-container"
+)[0];
+const textbtn = document.getElementsByClassName("text-btn")[0];
+
+textbtn.addEventListener("click", () => {
+  footertoptextcontainer.classList.toggle("active");
+});
+
+function addOne() {
+  const loadMoredisplay = document.getElementById("loadMore-display");
+  const inside = loadMoredisplay.innerHTML;
+  const number = parseInt(inside);
+  const larger = number + 1;
+  loadMoredisplay.innerHTML = larger;
 }
-
