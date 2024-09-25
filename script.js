a26409f871a3f2277a9360a77fae7602a26813e2
hdfillmizle <119707352+hdfillmizle@users.noreply.github.com>
2024-09-25 12:37:47 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const drop_down = document.getElementsByClassName("drop-down")[0];
-const down = document.getElementsByClassName("down")[0];
-
-drop_down.addEventListener("click", () => {
-  down.classList.toggle("active");
-});
